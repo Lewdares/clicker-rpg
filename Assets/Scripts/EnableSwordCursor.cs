41fa5bcbb8c79cf8755a7be6e50a81a9e744e013
Lewdares <lewdaresdev@gmail.com>
2019-09-28 19:32:31 -0300

    Buttons for SHIELD and FIRE!
    They do nothing for now.

--- Assets/Scripts/EnableSwordCursor.cs
+++ Assets/Scripts/EnableSwordCursor.cs
@@ -7,16 +7,24 @@ public class EnableSwordCursor : MonoBehaviour
 	
 	public static GameObject mouseSword;
 	public static GameObject mouseCharm;
+	public static GameObject mouseShield;
+	public static GameObject mouseFire;
 	public static GameObject swordHolder;
 	public static GameObject charmHolder;
+	public static GameObject shieldHolder;
+	public static GameObject fireHolder;
 	
     // Start is called before the first frame update
     void Start()
     {
         mouseSword = GameObject.Find("sword");
 		mouseCharm = GameObject.Find("charm");
+		mouseShield = GameObject.Find("shield");
+		mouseFire = GameObject.Find("fire");
 		swordHolder = GameObject.Find("swordHolder");
 		charmHolder = GameObject.Find("charmHolder");
+		shieldHolder = GameObject.Find("shieldHolder");
+		fireHolder = GameObject.Find("fireHolder");
     }
 
     // Update is called once per frame
@@ -26,16 +34,26 @@ public class EnableSwordCursor : MonoBehaviour
 			if (MouseCursor.isSwordActive == false) {
 				MouseCursor.isSwordActive = true;
 				
-				//stop holding other items
+				//** stop holding other items **
 				if (MouseCursor.isCharmActive == true) {
 					MouseCursor.isCharmActive = false;
-					mouseCharm.transform.position = new Vector3(charmHolder.transform.position.x, this.transform.position.y, this.transform.position.z); //TO-DO: it shouldn't be this.transform.position
+					mouseCharm.transform.position = new Vector3(charmHolder.transform.position.x, charmHolder.transform.position.y, this.transform.position.z); 
+				}
+				
+				if (MouseCursor.isShieldActive == true) {
+					MouseCursor.isShieldActive = false;
+					mouseShield.transform.position = new Vector3(shieldHolder.transform.position.x, shieldHolder.transform.position.y, this.transform.position.z); 
+				}
+				
+				if (MouseCursor.isFireActive == true) {
+					MouseCursor.isFireActive = false;
+					mouseFire.transform.position = new Vector3(fireHolder.transform.position.x, fireHolder.transform.position.y, this.transform.position.z); 
 				}
 				Debug.Log("now holding sword");
 			}
 			else {
 				MouseCursor.isSwordActive = false;
-				mouseSword.transform.position = new Vector3(swordHolder.transform.position.x, this.transform.position.y, this.transform.position.z);
+				mouseSword.transform.position = new Vector3(swordHolder.transform.position.x, swordHolder.transform.position.y, this.transform.position.z);
 				Debug.Log("now not holding sword");
 			}
 		}
@@ -43,17 +61,81 @@ public class EnableSwordCursor : MonoBehaviour
 			if (MouseCursor.isCharmActive == false) {
 				MouseCursor.isCharmActive = true;
 				
-				//stop holding other items
+				//** stop holding other items **
 				if (MouseCursor.isSwordActive == true) {
 					MouseCursor.isSwordActive = false;
-					mouseSword.transform.position = new Vector3(swordHolder.transform.position.x, this.transform.position.y, this.transform.position.z); //TO-DO: it shouldn't be this.transform.position
+					mouseSword.transform.position = new Vector3(swordHolder.transform.position.x, swordHolder.transform.position.y, this.transform.position.z); 
+				}
+				
+				if (MouseCursor.isShieldActive == true) {
+					MouseCursor.isShieldActive = false;
+					mouseShield.transform.position = new Vector3(shieldHolder.transform.position.x, shieldHolder.transform.position.y, this.transform.position.z); 
+				}
+				
+				if (MouseCursor.isFireActive == true) {
+					MouseCursor.isFireActive = false;
+					mouseFire.transform.position = new Vector3(fireHolder.transform.position.x, fireHolder.transform.position.y, this.transform.position.z); 
 				}
 				Debug.Log("now holding charm");
 			}
 			else {
 				MouseCursor.isCharmActive = false;
-				mouseCharm.transform.position = new Vector3(charmHolder.transform.position.x, this.transform.position.y, this.transform.position.z);
+				mouseCharm.transform.position = new Vector3(charmHolder.transform.position.x, charmHolder.transform.position.y, this.transform.position.z);
 				Debug.Log("now not holding charm");
+			}
+		}
+		else if (this.gameObject.name == "shieldHolder") {
+			if (MouseCursor.isShieldActive == false) {
+				MouseCursor.isShieldActive = true;
+				
+				//** stop holding other items **
+				if (MouseCursor.isSwordActive == true) {
+					MouseCursor.isSwordActive = false;
+					mouseSword.transform.position = new Vector3(swordHolder.transform.position.x, swordHolder.transform.position.y, this.transform.position.z); 
+				}
+				
+				if (MouseCursor.isCharmActive == true) {
+					MouseCursor.isCharmActive = false;
+					mouseCharm.transform.position = new Vector3(charmHolder.transform.position.x, charmHolder.transform.position.y, this.transform.position.z); 
+				}
+				
+				if (MouseCursor.isFireActive == true) {
+					MouseCursor.isFireActive = false;
+					mouseFire.transform.position = new Vector3(fireHolder.transform.position.x, fireHolder.transform.position.y, this.transform.position.z); 
+				}
+				Debug.Log("now holding shield");
+			}
+			else {
+				MouseCursor.isShieldActive = false;
+				mouseShield.transform.position = new Vector3(shieldHolder.transform.position.x, shieldHolder.transform.position.y, this.transform.position.z);
+				Debug.Log("now not holding shield");
+			}
+		}
+		else if (this.gameObject.name == "fireHolder") {
+			if (MouseCursor.isFireActive == false) {
+				MouseCursor.isFireActive = true;
+				
+				//** stop holding other items **
+				if (MouseCursor.isSwordActive == true) {
+					MouseCursor.isSwordActive = false;
+					mouseSword.transform.position = new Vector3(swordHolder.transform.position.x, swordHolder.transform.position.y, this.transform.position.z); 
+				}
+				
+				if (MouseCursor.isShieldActive == true) {
+					MouseCursor.isShieldActive = false;
+					mouseShield.transform.position = new Vector3(shieldHolder.transform.position.x, shieldHolder.transform.position.y, this.transform.position.z); 
+				}
+				
+				if (MouseCursor.isCharmActive == true) {
+					MouseCursor.isCharmActive = false;
+					mouseCharm.transform.position = new Vector3(charmHolder.transform.position.x, charmHolder.transform.position.y, this.transform.position.z); 
+				}
+				Debug.Log("now holding fire");
+			}
+			else {
+				MouseCursor.isFireActive = false;
+				mouseFire.transform.position = new Vector3(fireHolder.transform.position.x, fireHolder.transform.position.y, this.transform.position.z);
+				Debug.Log("now not holding fire");
 			}
 		}
     }
--- Assets/Scripts/MouseCursor.cs
+++ Assets/Scripts/MouseCursor.cs
@@ -6,6 +6,8 @@ public class MouseCursor : MonoBehaviour
 {
 	public static bool isSwordActive = false;
 	public static bool isCharmActive = false;
+	public static bool isShieldActive = false;
+	public static bool isFireActive = false;
 	
     // Start is called before the first frame update
     void Start()
@@ -25,6 +27,20 @@ public class MouseCursor : MonoBehaviour
 		}
 		else if (isCharmActive == true) {
 			if (this.gameObject.name == "charm") {
+				Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				transform.position = cursorPos;
+				Cursor.visible = false;
+			}
+		}
+		else if (isShieldActive == true) {
+			if (this.gameObject.name == "shield") {
+				Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+				transform.position = cursorPos;
+				Cursor.visible = false;
+			}
+		}
+		else if (isFireActive == true) {
+			if (this.gameObject.name == "fire") {
 				Vector2 cursorPos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 				transform.position = cursorPos;
 				Cursor.visible = false;
