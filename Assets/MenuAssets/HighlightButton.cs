e58a230e881363d63cd250cd1214a6f848a0f965
Lewdares <lewdaresdev@gmail.com>
2019-09-30 11:32:37 -0300

    sorta good looking menu
    patreon and discord buttons now work, working on the actual "go and play the fucking game lol" button

--- Assets/MenuAssets/HighlightButton.cs
+++ Assets/MenuAssets/HighlightButton.cs
@@ -4,14 +4,30 @@ using UnityEngine;
 
 public class HighlightButton : MonoBehaviour
 {
-	private Color startcolor;
+	public GameObject transBar; //transition bar, you silly fuck
+	void Start()
+	{
+		transBar = GameObject.Find("TransitionBars");
+	}
 	void OnMouseEnter()
 	{
-		this.GetComponent<Animation>().Play("highlightbutton");
 		Debug.Log("hi friend!");
     }
     void OnMouseExit()
     {
         Debug.Log("bye :0");
     }
+	void OnMouseDown()
+	{
+		if (this.gameObject.name == "StartButton") {
+		transBar.GetComponent<Animation>().Play("transitionmenu"); 
+		}
+		else if (this.gameObject.name == "PatreonButton") {
+			//i want to elaborate more on this but whatever
+			Application.OpenURL("https://www.patreon.com/SlimeKitten");
+		}
+		else if (this.gameObject.name == "DiscordButton") {
+			Application.OpenURL("https://discord.gg/c6sTVZX");
+		}
+	}
 }
