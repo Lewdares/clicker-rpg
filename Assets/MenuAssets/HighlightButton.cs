06fd5c9531471ce12e9174930755267ee3c6a7bd
Lewdares <lewdaresdev@gmail.com>
2019-09-30 10:53:57 -0300

    menu basic shit

---
+++ Assets/MenuAssets/HighlightButton.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HighlightButton : MonoBehaviour
+{
+	private Color startcolor;
+	void OnMouseEnter()
+	{
+		this.GetComponent<Animation>().Play("highlightbutton");
+		Debug.Log("hi friend!");
+    }
+    void OnMouseExit()
+    {
+        Debug.Log("bye :0");
+    }
+}
